de9b120281601deee663964d27e3f2fdcbdafd06
Andrey Arthur <andrey_arthur1@outlook.com>
2024-06-23 18:03:15 -0300

    implement the lexer

---
+++ lexing/lexer.go
@@ -0,0 +1,189 @@
+package lexing
+
+import "bytes"
+
+func isDigit(character byte) bool {
+	return character >= '0' && character <= '9'
+}
+
+func isAlphabetic(character byte) bool {
+	return (character >= 'A' && character <= 'Z') ||
+		(character >= 'a' && character <= 'z') ||
+		character == '_'
+}
+
+func isAlphanumeric(character byte) bool {
+	return isDigit(character) || isAlphabetic(character)
+}
+
+type Lexer struct {
+	content  string
+	position int
+	current  byte
+}
+
+func NewLexer(content string) *Lexer {
+	return &Lexer{
+		content:  content,
+		position: 0,
+		current:  content[0],
+	}
+}
+
+func (lexer *Lexer) advance() {
+	if lexer.position+1 >= len(lexer.content) {
+		lexer.current = '\x00'
+		return
+	}
+	lexer.position += 1
+	lexer.current = lexer.content[lexer.position]
+}
+
+func (lexer *Lexer) peek() byte {
+	if lexer.position+1 >= len(lexer.content) {
+		return '\x00'
+	}
+	return lexer.content[lexer.position+1]
+}
+
+func (lexer *Lexer) skipWhitespaces() {
+	for lexer.current == ' ' ||
+		lexer.current == '\n' ||
+		lexer.current == '\r' ||
+		lexer.current == '\t' {
+		lexer.advance()
+	}
+}
+
+func (lexer *Lexer) collectCurrent(tokenType TokenType) *Token {
+	token := NewToken(tokenType, string(lexer.current))
+	lexer.advance()
+	return token
+}
+
+func (lexer *Lexer) collectWithNext(tokenType TokenType) *Token {
+	token := NewToken(tokenType, string(lexer.current)+string(lexer.peek()))
+	lexer.advance()
+	lexer.advance()
+	return token
+}
+
+func (lexer *Lexer) collectIntegerLiteral() *Token {
+	var buffer bytes.Buffer
+
+	for isDigit(lexer.current) {
+		buffer.WriteByte(lexer.current)
+		lexer.advance()
+	}
+
+	return NewToken(TOKEN_INTEGER, buffer.String())
+}
+
+func (lexer *Lexer) collectStringLiteral() *Token {
+	var buffer bytes.Buffer
+
+	lexer.advance()
+	// TODO: handle escaope characters
+	for lexer.current != '"' {
+		buffer.WriteByte(lexer.current)
+		lexer.advance()
+	}
+	lexer.advance()
+
+	return NewToken(TOKEN_STRING, buffer.String())
+}
+
+func (lexer *Lexer) collectIdentifierOrKeyword() *Token {
+	var buffer bytes.Buffer
+
+	for isAlphanumeric(lexer.current) {
+		buffer.WriteByte(lexer.current)
+		lexer.advance()
+	}
+
+	text := buffer.String()
+
+	var tokenType TokenType
+
+	switch text {
+	case "let":
+		tokenType = TOKEN_LET
+	case "fn":
+		tokenType = TOKEN_FUNCTION
+	case "return":
+		tokenType = TOKEN_RETURN
+	case "if":
+		tokenType = TOKEN_IF
+	case "else":
+		tokenType = TOKEN_ELSE
+	default:
+		tokenType = TOKEN_IDENTIFIER
+	}
+
+	return NewToken(tokenType, text)
+}
+
+func (lexer *Lexer) Next() *Token {
+	lexer.skipWhitespaces()
+
+	switch lexer.current {
+	case '\x00':
+		return lexer.collectCurrent(TOKEN_EOF)
+	case '=':
+		if lexer.peek() == '=' {
+			return lexer.collectWithNext(TOKEN_EQUALS)
+		}
+		return lexer.collectCurrent(TOKEN_ASSIGN)
+	case '+':
+		return lexer.collectCurrent(TOKEN_PLUS)
+	case '-':
+		return lexer.collectCurrent(TOKEN_MINUS)
+	case '*':
+		return lexer.collectCurrent(TOKEN_ASTERISK)
+	case '/':
+		return lexer.collectCurrent(TOKEN_SLASH)
+	case '!':
+		if lexer.peek() == '=' {
+			return lexer.collectWithNext(TOKEN_NOT_EQUALS)
+		}
+		return lexer.collectCurrent(TOKEN_BANG)
+	case '>':
+		if lexer.peek() == '=' {
+			return lexer.collectWithNext(TOKEN_GREATER_OR_EQUALS)
+		}
+		return lexer.collectCurrent(TOKEN_GREATER)
+	case '<':
+		if lexer.peek() == '=' {
+			return lexer.collectWithNext(TOKEN_LESS_OR_EQUALS)
+		}
+		return lexer.collectCurrent(TOKEN_LESS)
+	case '(':
+		return lexer.collectCurrent(TOKEN_OPEN_PAREN)
+	case ')':
+		return lexer.collectCurrent(TOKEN_CLOSE_PAREN)
+	case '{':
+		return lexer.collectCurrent(TOKEN_OPEN_BRACE)
+	case '}':
+		return lexer.collectCurrent(TOKEN_CLOSE_BRACE)
+	case '[':
+		return lexer.collectCurrent(TOKEN_OPEN_BRACKET)
+	case ']':
+		return lexer.collectCurrent(TOKEN_CLOSE_BRACKET)
+	case ',':
+		return lexer.collectCurrent(TOKEN_COMMA)
+	case ':':
+		return lexer.collectCurrent(TOKEN_COLON)
+	case ';':
+		return lexer.collectCurrent(TOKEN_SEMICOLON)
+	case '"':
+		return lexer.collectStringLiteral()
+	default:
+		if isDigit(lexer.current) {
+			return lexer.collectIntegerLiteral()
+		}
+		if isAlphabetic(lexer.current) {
+			return lexer.collectIdentifierOrKeyword()
+		}
+		return lexer.collectCurrent(TOKEN_ILLEGAL)
+	}
+}
---
+++ tests/lexing/lexer_test.go
@@ -0,0 +1,166 @@
+package lexing_test
+
+import (
+	"monkey/lexing"
+	"testing"
+)
+
+func TestLexer(t *testing.T) {
+	content := `let a = 2 + 2 - 4;
+
+let b = 2 * 2 / 4;
+
+let function = fn () {
+    return !true != false;
+};
+
+let c = if (function) true else false;
+
+2 <= 2;
+2 < 3;
+2 >= 2;
+2 > 1;
+5 == 6;
+
+"Hello, World";
+
+let d = {"hello": "world"};
+
+d["hello"];
+
+let e = [1, 2, 3, 4, 5];
+
+e[4] == 5;
+
+@;
+`
+
+	expectations := []struct {
+		tokenType lexing.TokenType
+		value     string
+	}{
+		{lexing.TOKEN_LET, "let"},
+		{lexing.TOKEN_IDENTIFIER, "a"},
+		{lexing.TOKEN_ASSIGN, "="},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_PLUS, "+"},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_MINUS, "-"},
+		{lexing.TOKEN_INTEGER, "4"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_LET, "let"},
+		{lexing.TOKEN_IDENTIFIER, "b"},
+		{lexing.TOKEN_ASSIGN, "="},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_ASTERISK, "*"},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_SLASH, "/"},
+		{lexing.TOKEN_INTEGER, "4"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_LET, "let"},
+		{lexing.TOKEN_IDENTIFIER, "function"},
+		{lexing.TOKEN_ASSIGN, "="},
+		{lexing.TOKEN_FUNCTION, "fn"},
+		{lexing.TOKEN_OPEN_PAREN, "("},
+		{lexing.TOKEN_CLOSE_PAREN, ")"},
+		{lexing.TOKEN_OPEN_BRACE, "{"},
+		{lexing.TOKEN_RETURN, "return"},
+		{lexing.TOKEN_BANG, "!"},
+		{lexing.TOKEN_IDENTIFIER, "true"},
+		{lexing.TOKEN_NOT_EQUALS, "!="},
+		{lexing.TOKEN_IDENTIFIER, "false"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_CLOSE_BRACE, "}"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_LET, "let"},
+		{lexing.TOKEN_IDENTIFIER, "c"},
+		{lexing.TOKEN_ASSIGN, "="},
+		{lexing.TOKEN_IF, "if"},
+		{lexing.TOKEN_OPEN_PAREN, "("},
+		{lexing.TOKEN_IDENTIFIER, "function"},
+		{lexing.TOKEN_CLOSE_PAREN, ")"},
+		{lexing.TOKEN_IDENTIFIER, "true"},
+		{lexing.TOKEN_ELSE, "else"},
+		{lexing.TOKEN_IDENTIFIER, "false"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_LESS_OR_EQUALS, "<="},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_LESS, "<"},
+		{lexing.TOKEN_INTEGER, "3"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_GREATER_OR_EQUALS, ">="},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_GREATER, ">"},
+		{lexing.TOKEN_INTEGER, "1"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_INTEGER, "5"},
+		{lexing.TOKEN_EQUALS, "=="},
+		{lexing.TOKEN_INTEGER, "6"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_STRING, "Hello, World"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_LET, "let"},
+		{lexing.TOKEN_IDENTIFIER, "d"},
+		{lexing.TOKEN_ASSIGN, "="},
+		{lexing.TOKEN_OPEN_BRACE, "{"},
+		{lexing.TOKEN_STRING, "hello"},
+		{lexing.TOKEN_COLON, ":"},
+		{lexing.TOKEN_STRING, "world"},
+		{lexing.TOKEN_CLOSE_BRACE, "}"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_IDENTIFIER, "d"},
+		{lexing.TOKEN_OPEN_BRACKET, "["},
+		{lexing.TOKEN_STRING, "hello"},
+		{lexing.TOKEN_CLOSE_BRACKET, "]"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_LET, "let"},
+		{lexing.TOKEN_IDENTIFIER, "e"},
+		{lexing.TOKEN_ASSIGN, "="},
+		{lexing.TOKEN_OPEN_BRACKET, "["},
+		{lexing.TOKEN_INTEGER, "1"},
+		{lexing.TOKEN_COMMA, ","},
+		{lexing.TOKEN_INTEGER, "2"},
+		{lexing.TOKEN_COMMA, ","},
+		{lexing.TOKEN_INTEGER, "3"},
+		{lexing.TOKEN_COMMA, ","},
+		{lexing.TOKEN_INTEGER, "4"},
+		{lexing.TOKEN_COMMA, ","},
+		{lexing.TOKEN_INTEGER, "5"},
+		{lexing.TOKEN_CLOSE_BRACKET, "]"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_IDENTIFIER, "e"},
+		{lexing.TOKEN_OPEN_BRACKET, "["},
+		{lexing.TOKEN_INTEGER, "4"},
+		{lexing.TOKEN_CLOSE_BRACKET, "]"},
+		{lexing.TOKEN_EQUALS, "=="},
+		{lexing.TOKEN_INTEGER, "5"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_ILLEGAL, "@"},
+		{lexing.TOKEN_SEMICOLON, ";"},
+		{lexing.TOKEN_EOF, "\x00"},
+	}
+
+	lexer := lexing.NewLexer(content)
+
+	for index, expectation := range expectations {
+		token := lexer.Next()
+
+		if token.Type != expectation.tokenType ||
+			token.Value != expectation.value {
+			t.Fatalf(
+				"[%d] Expected token %q of type %d, found token %q of type %d.",
+				index,
+				expectation.value,
+				expectation.tokenType,
+				token.Value,
+				token.Type,
+			)
+		}
+	}
+}
